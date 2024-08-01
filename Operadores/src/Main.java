018f3ee2da876b8ab07a22f18ed0427747a81fbf
Guilherme Silva <sgsouza.173@gmail.com>
2024-08-01 14:18:27 -0300

    Tipos primitivos e operadores

---
+++ Operadores/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        /**String nomeCompleto = "Linguagem" + "Java";
+        System.out.println(nomeCompleto);**/ ///Mostra LinguagemJava
+
+        String concatenacao = "?";
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+    }
+}
