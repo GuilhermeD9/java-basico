55805ffc015967042a3142fe94bc13e3f4ab9bce
Guilherme Silva <sgsouza.173@gmail.com>
2024-08-22 14:28:43 -0300

    Implementação da abstração
    POO!!

--- MsnMensager/src/FacebookMensager.java
+++ MsnMensager/src/FacebookMensager.java
@@ -1,3 +1,13 @@
 public class FacebookMensager extends ServicoMensagemInstatanea {
 
+    @Override
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo Facebook Mensager");
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo Facebook Mensager");
+    }
+
 }
--- MsnMensager/src/MsnMesseger.java
+++ MsnMensager/src/MsnMesseger.java
@@ -1,3 +1,13 @@
 public class MsnMesseger extends ServicoMensagemInstatanea {
 
+    @Override
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo MSN");
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo MSN");
+    }
+
 }
