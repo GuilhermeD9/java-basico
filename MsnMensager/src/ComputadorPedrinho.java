4ccd781b35960ed810f8d9fc2046ddd368a6d2e0
Guilherme Silva <sgsouza.173@gmail.com>
2024-08-22 14:14:02 -0300

    Treinando POO Java!
    Polimorfismo

---
+++ MsnMensager/src/ComputadorPedrinho.java
@@ -0,0 +1,8 @@
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        MsnMesseger msn = new MsnMesseger();
+        
+        msn.enviarMensagem();
+        msn.receberMensagem();
+    }
+}
